54108e9c4efa437297bec5712f12f71981532eb4
Kristian Nielsen <100754003+Kristian987@users.noreply.github.com>
2022-05-09 15:53:48 +0200

    routes added

--- Angular_Website/src/app/app.module.ts
+++ Angular_Website/src/app/app.module.ts
@@ -4,12 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { MenuComponent } from './menu/menu.component';
+import { BodytherapyComponent } from './bodytherapy/bodytherapy/bodytherapy.component';
+import { AboutComponent } from './about/about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { PricesComponent } from './prices/prices.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    MenuComponent
+    MenuComponent,
+    BodytherapyComponent,
+    AboutComponent,
+    ContactComponent,
+    PricesComponent
   ],
   imports: [
     BrowserModule
---
+++ Angular_Website/src/app/bodytherapy/bodytherapy.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bodytherapy',
+  templateUrl: './bodytherapy.component.html',
+  styleUrls: ['./bodytherapy.component.css']
+})
+export class BodytherapyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
