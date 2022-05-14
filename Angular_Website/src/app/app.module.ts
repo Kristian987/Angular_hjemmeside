b448d8d9a7fb23823df0e804ac94ee28adbc7e62
Kristian Nielsen <100754003+Kristian987@users.noreply.github.com>
2022-05-14 11:01:13 +0200

    init + about

--- Angular_Website/src/app/app.module.ts
+++ Angular_Website/src/app/app.module.ts
@@ -1,26 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { MenuComponent } from './menu/menu.component';
-import { BodytherapyComponent } from './bodytherapy/bodytherapy/bodytherapy.component';
-import { AboutComponent } from './about/about/about.component';
+import { AboutComponent } from './about/about.component';
+import { BodytherapyComponent } from './bodytherapy/bodytherapy.component';
 import { ContactComponent } from './contact/contact.component';
+import { MenuComponent } from './menu/menu.component';
 import { PricesComponent } from './prices/prices.component';
+
+const appRoutes: Routes = [
+  { path: 'about', component: AboutComponent },
+  { path: 'bodytherapy', component: BodytherapyComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'menu', component: MenuComponent },
+  { path: 'prices', component: PricesComponent },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    MenuComponent,
-    BodytherapyComponent,
     AboutComponent,
+    BodytherapyComponent,
     ContactComponent,
+    MenuComponent,
     PricesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
